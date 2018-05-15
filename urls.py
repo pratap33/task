6da1eafa555f0590565d0aefc05c87851a4c277f
pratap33 <39182710+pratap33@users.noreply.github.com>
2018-05-15 15:32:43 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from userapp.views import Homepage,Loginpage,Showpage
-from . import views
-app_name='userapp'
-urlpatterns=  [
-    url(r'^homepage$',Homepage.as_view()),
-    url(r'^loginpage$',Loginpage.as_view()),
-    url(r'^showpage$',Showpage.as_view()),
-]
