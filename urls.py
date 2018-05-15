8dc68a71f900a6ff73c1baabd0bef2029ba09908
pratap33 <39182710+pratap33@users.noreply.github.com>
2018-05-15 15:39:24 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from userapp.views import Homepage,Loginpage,Showpage
+from . import views
+app_name='userapp'
+urlpatterns=  [
+    url(r'^homepage$',Homepage.as_view()),
+    url(r'^loginpage$',Loginpage.as_view()),
+    url(r'^showpage$',Showpage.as_view()),
+    url(r'^insert$',views.insert, name='insert'),
+]
